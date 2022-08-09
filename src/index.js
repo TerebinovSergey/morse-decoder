307ef098e908d1ba6f714f5b3846919cca51a3dd
TerebinovSergey <neznacomec94@gmail.com>
2022-08-09 23:36:26 +0400

    solved task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,58 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = [];
+    let charCode = '';
+    let count = 1;
+    
+    for (let i = 0; i < expr.length + 1; i++) {
+        if (count > 10) {
+            count = 1;
+            arr.push(charCode);
+            charCode = '';
+        }
+        
+        charCode += expr[i];
+        count += 1    
+    }
+    
+    let lastChar = '';
+    let strings = [];
+    let str = [];
+    arr.forEach(item => {
+        str = [];
+        if (item.replace(/\*/g, '') === '') {
+            str.push(' ');
+        } else {
+            item.split('').forEach((i, ind) => {
+                
+                if (lastChar === '') {
+                    lastChar = i;
+                } else if (i === '1' && lastChar === '0') {
+                    lastChar = '1';
+                } else if (lastChar === '1') {
+                    let add = (i === '0') ? '.' : '-';
+                    str.push(add);
+                    lastChar = '';
+                }
+            });
+        }
+        
+        strings.push(str)
+        
+    });
+    
+    let result = '';
+    strings.forEach(item => {
+        str = item.join('');
+        if (str === ' ') {
+            result += str;
+        } else {
+            result += MORSE_TABLE[str];    
+        }
+    })
+    
+    return result;
 }
 
 module.exports = {
